858071bc023fac4277f43aadf70c47260d9e399a
Sebastiang509 <nguyensebastian9@gmail.com>
2025-03-17 21:24:45 -0500

    Added comments to explain functions in todo_service.fs

--- backend/todo_service.fs
+++ backend/todo_service.fs
@@ -3,11 +3,14 @@ module TodoService
 open Giraffe
 open Microsoft.AspNetCore.Http
 
+// List of sample todos
 let mutable todos = ["Finish CS 510 project"; "Submit assignment"]
 
+// API handler to fetch todos
 let getTodosHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
         json todos next ctx
+
 
 let addTodoHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
